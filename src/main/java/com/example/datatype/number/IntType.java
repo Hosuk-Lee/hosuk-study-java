59ca93d81f97d1701a4f095e5991e88faf1ed1b1
hosuk <hosuk1497@gmail.com>
2022-08-30 23:18:05 +0900

    java gradle basic init

---
+++ src/main/java/com/example/datatype/number/IntType.java
@@ -0,0 +1,21 @@
+package com.example.datatype.number;
+
+public class IntType {
+
+    public static void test(){
+        int a;
+        
+        System.out.printf("Integer의 크기 : %d\n",Integer.BYTES);
+
+        String b = "병신";
+        System.out.printf(b);
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
